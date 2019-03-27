9a855a48c0d09058d0e1a4a73ee263459de98a01
Marc Davies <marcdaviesriot@gmail.com>
2019-03-27 00:01:54 -0700

    Add function to toggle between light and dark themes upon clicking a button

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#initial-light").toggle();
+    $("#show-dark").toggle();
+  });
+});
+
+/*$(document).ready(function() {
   $("button#black").click(function() {
     $("body").removeClass();
     $("body").addClass("dark-background");
-  });
+  });*/
 
   /*$("button#yellow").click(function() {
     $("body").removeClass();
@@ -12,5 +19,5 @@ $(document).ready(function() {
   /*$("button#red").click(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
-  });*/
-});
+  });
+});*/
